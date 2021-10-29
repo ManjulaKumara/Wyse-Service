0170e3bf226f9a723f3ada600d5b6dbd197bfd57
Manjula Kulathunga <wysheit@gmail.com>
2021-10-29 15:23:20 +0530

    template integrate

---
+++ resources/views/layouts/includes/primary-menu.blade.php
@@ -0,0 +1,71 @@
+<!--begin::Aside-->
+<div id="kt_aside" class="aside bg-primary" data-kt-drawer="true" data-kt-drawer-name="aside" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="auto" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_aside_toggle">
+    <!--begin::Logo-->
+    <div class="aside-logo d-none d-lg-flex flex-column align-items-center flex-column-auto py-8" id="kt_aside_logo">
+        <a href="../../demo4/dist/index.html">
+            <img alt="Logo" src="{{url('assets/media/logos/logo-demo4.svg')}}" class="h-55px" />
+        </a>
+    </div>
+    <!--end::Logo-->
+    <!--begin::Nav-->
+    <div class="aside-nav d-flex flex-column align-lg-center flex-column-fluid w-100 pt-5 pt-lg-0" id="kt_aside_nav">
+        <!--begin::Primary menu-->
+        <div id="kt_aside_menu" class="menu menu-column menu-title-gray-600 menu-state-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-500 fw-bold fs-6" data-kt-menu="true">
+            <div class="menu-item py-3">
+                <a class="menu-link active menu-center" href="../../demo4/dist/index.html" title="Dashboard" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss="click" data-bs-placement="right">
+                    <span class="menu-icon me-0">
+                        <i class="bi bi-house fs-2"></i>
+                    </span>
+                </a>
+            </div>
+            <div data-kt-menu-trigger="click" data-kt-menu-placement="right-start" class="menu-item py-3">
+                <a class="menu-link menu-center" href="../../demo4/dist/account/overview.html">
+                    <span class="menu-icon me-0">
+                        <i class="bi bi-file-earmark-lock fs-2"></i>
+                    </span>
+                </a>
+            </div>
+            <div data-kt-menu-trigger="click" data-kt-menu-placement="right-start" class="menu-item py-3">
+                <a class="menu-link menu-center" href="../../demo4/dist/account/overview.html">
+                    <span class="menu-icon me-0">
+                        <i class="bi bi-shield-check fs-2"></i>
+                    </span>
+                </a>
+            </div>
+            <div data-kt-menu-trigger="click" data-kt-menu-placement="right-start" class="menu-item py-3">
+                <a class="menu-link menu-center" href="../../demo4/dist/account/overview.html">
+                    <span class="menu-icon me-0">
+                        <i class="bi bi-chat-square-text fs-2"></i>
+                    </span>
+                </a>
+            </div>
+            <div data-kt-menu-trigger="click" data-kt-menu-placement="right-start" class="menu-item py-3">
+                <a class="menu-link menu-center" href="../../demo4/dist/account/overview.html">
+                    <span class="menu-icon me-0">
+                        <i class="bi bi-people fs-2"></i>
+                    </span>
+                </a>
+            </div>
+            <div data-kt-menu-trigger="click" data-kt-menu-placement="right-start" class="menu-item py-3">
+                <span class="menu-link menu-center" title="General Pages" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss="click" data-bs-placement="right">
+                    <span class="menu-icon me-0">
+                        <i class="bi bi-file-text fs-2"></i>
+                    </span>
+                </span>
+
+            </div>
+            <div data-kt-menu-trigger="click" data-kt-menu-placement="right-start" class="menu-item py-3">
+                <span class="menu-link menu-center" title="Resources" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss="click" data-bs-placement="right">
+                    <span class="menu-icon me-0">
+                        <i class="bi bi-gear fs-2"></i>
+                    </span>
+                </span>
+
+            </div>
+        </div>
+        <!--end::Primary menu-->
+    </div>
+    <!--end::Nav-->
+    <!--end::Footer-->
+</div>
+<!--end::Aside-->
